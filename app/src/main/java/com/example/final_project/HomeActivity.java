d3be26db2e3687b5ae432bcf247a575347ae9b1c
danielle <danielle31333133@gmail.com>
2022-07-22 10:08:27 +0300

    code review

--- app/src/main/java/com/example/final_project/HomeActivity.java
+++ app/src/main/java/com/example/final_project/HomeActivity.java
@@ -1,17 +1,24 @@
 package com.example.final_project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HomeActivity extends AppCompatActivity implements WeekFragment.WeekFragmentListener{
+//public class HomeActivity extends AppCompatActivity implements WeekFragment.WeekFragmentListener{
+public class HomeActivity extends MainActivity implements WeekFragment.WeekFragmentListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Content View
         setContentView(R.layout.activity_home);
+
     }
+
 
     @Override
     public void OnClickEvent(String day) {
