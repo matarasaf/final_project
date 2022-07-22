4edb24c54e398f7a50e0bb9997ebbb23e1fbb51e
danielle <danielle31333133@gmail.com>
2022-07-22 10:11:28 +0300

    check if oncreateview relevant

--- app/src/main/java/com/example/final_project/DayFragment.java
+++ app/src/main/java/com/example/final_project/DayFragment.java
@@ -1,5 +1,7 @@
 package com.example.final_project;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +13,20 @@ import androidx.fragment.app.Fragment;
 
 public class DayFragment extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+       /* SharedPreferences sharedPreferences = getActivity().getSharedPreferences("FONT_SIZE", Context.MODE_PRIVATE);
+        //If size not declare initialize to empty
+        String size = sharedPreferences.getString("SIZE", "");
+        if (size.equals(" small "))
+            getContext().setTheme(R.style.small_text);
+        else if (size.equals(" medium "))
+            getContext().setTheme(R.style.medium_text);
+        else if (size.equals(" large "))
+            getContext().setTheme(R.style.large_text);*/
+
 
         return inflater.inflate(R.layout.fragment_day, container,false);
     }
