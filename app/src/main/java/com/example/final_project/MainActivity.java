2802e5f900647547bcb6e32819237b96d8f2fd23
danielle <danielle31333133@gmail.com>
2022-07-22 10:07:37 +0300

    adding home menu + responsible to font change

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -1,26 +1,25 @@
 package com.example.final_project;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-import android.app.ProgressDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPrefs = getSharedPreferences("FONT_SIZE", MODE_PRIVATE);
+
+        //The size variable contains the font size
+        //setTheme must be called before setContentView
+        String size = sharedPrefs.getString("SIZE", "");
+        if (size.equals(" small "))
+            setTheme(R.style.small_text);
+        else if (size.equals(" medium "))
+            setTheme(R.style.medium_text);
+        else if (size.equals(" large "))
+            setTheme(R.style.large_text);
+
         setContentView(R.layout.activity_main);
 
         ViewPager viewPager = findViewById(R.id.viewPager);
@@ -37,6 +49,18 @@ public class MainActivity extends AppCompatActivity {
         pagerAdapter.addFragment(new RegisterFragment());
 
         viewPager.setAdapter(pagerAdapter);
+    }
+
+    //update settings- change font size
+    //overridePendingTransition call immediately after finish()/startActivity() function
+    //      for attention transit between activity
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
     }
 
     class AuthenticationPagerAdapter extends FragmentPagerAdapter {
@@ -58,6 +82,77 @@ public class MainActivity extends AppCompatActivity {
 
         void addFragment(Fragment fragment) {
             fragmentList.add(fragment);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu,menu);
+        return true;
+    }
+
+    //Menu buttons options implementation
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                startActivity(new Intent(this,SettingsActivity.class));
+                return true;
+            case R.id.action_exit:
+                showDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showDialog() {
+        DialogFragment newFragment = MainActivity.MyAlertDialogFragment.newInstance();
+        newFragment.show(getSupportFragmentManager(),"dialog");
+    }
+
+    public static void doPositiveClick() {
+        System.exit(0);
+    }
+
+
+    public static void doNegativeClick(DialogInterface dialog) {
+        dialog.dismiss();
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MainActivity.MyAlertDialogFragment newInstance() {
+            MainActivity.MyAlertDialogFragment frag = new MainActivity
+                    .MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            frag.setArguments(args);
+            return frag;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            return new AlertDialog.Builder(getActivity())
+                    .setIcon(R.drawable.ic_launcher)
+                    .setTitle("Closing the application")
+                    .setMessage("Are you sure? ")
+                    .setPositiveButton("Yes",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    MainActivity.doPositiveClick();
+                                }
+                            }
+                    )
+                    .setNegativeButton("No",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    MainActivity.doNegativeClick(dialog);
+                                }
+                            }
+                    )
+                    .create();
+
         }
     }
 }
