a83fd9ba13b86cacf25954b01c937c4b60830465
danielle <danielle31333133@gmail.com>
2022-07-25 17:39:35 +0300

    design

--- app/src/main/java/com/example/final_project/HomeActivity.java
+++ app/src/main/java/com/example/final_project/HomeActivity.java
@@ -1,23 +1,26 @@
 package com.example.final_project;
 
+import android.app.AlarmManager;
+import android.app.AliasActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HomeActivity extends AppCompatActivity implements WeekFragment.WeekFragmentListener{
-
+public class HomeActivity extends BaseActivity implements WeekFragment.WeekFragmentListener{
     String day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         //Content View
         setContentView(R.layout.activity_home);
-
     }
 
 
@@ -30,13 +33,13 @@ public class HomeActivity extends AppCompatActivity implements WeekFragment.Week
                 .replace(R.id.fragContainer, DayFragment.class,null,"DAYFRAG")
                 .addToBackStack(null)
                 .commit();
-        getSupportFragmentManager().executePendingTransactions();
-
+        try {
+            getSupportFragmentManager().executePendingTransactions();
+        }catch (IndexOutOfBoundsException e) { e.printStackTrace();}
         frag = (DayFragment) getSupportFragmentManager().findFragmentByTag("DAYFRAG");
-
         frag.onNewClick(day);
-
     }
+
 
     public String getDay(){
         return day;
