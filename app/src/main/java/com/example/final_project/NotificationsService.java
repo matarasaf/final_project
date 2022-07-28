6b7eb70cc96e3a83518f8c27992ac06acf48e418
danielle <danielle31333133@gmail.com>
2022-07-28 18:32:46 +0300

    for notification

---
+++ app/src/main/java/com/example/final_project/NotificationsService.java
@@ -0,0 +1,69 @@
+package com.example.final_project;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class NotificationsService extends Service {
+
+    public static final String[] daysInWeek = {
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday"
+
+    };
+
+    public static final String N_CHANNEL_ID = "MY_CHANNEL";
+    private void createNotificationChannel() {
+        CharSequence name = "MY_CHANNEL";
+        String description = "Some channel";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(N_CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        setLessonsAlarms();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void setLessonsAlarms(){
+        // for each day of week set lesson alarms
+        for(String day  : daysInWeek) {
+            ArrayList<String> lessonData = FileManager.getDataFromDb(getApplicationContext(),day);
+            //getLessonsFromDb get false- not adding lesson to class list
+            ArrayList<Lesson> lessons = FileManager.getLessonsFromDb(lessonData,day,false);
+            for(Lesson lesson : lessons) {
+                if(lesson.getAttendance())
+                    NotificationAppManager.setLessonNotification(getApplicationContext(), lesson);
+            }
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+}
