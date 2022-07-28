b22924a8f531439ae1fd9ed6f425dd5185b714a3
danielle <danielle31333133@gmail.com>
2022-07-28 18:31:28 +0300

    initialize notification

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -1,30 +1,17 @@
 package com.example.final_project;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
-import android.widget.Toolbar;
+
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Timer;
-import java.util.TimerTask;
+
 
 
 public class MainActivity extends BaseActivity {
@@ -42,34 +29,14 @@ public class MainActivity extends BaseActivity {
 
         viewPager.setAdapter(pagerAdapter);
 
-
+        Intent i = new Intent(this,NotificationsService.class);
+        startService(i); //For notifications about classes with mandatory attendance
 
         // Sets the alarm for all the lessons on the system
         // on first app entrance
-        setLessonsAlarms();
     }
 
 
-    private void setLessonsAlarms(){
-        String[] daysInWeek = {
-                "Sunday",
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday"
-
-        };
-        // for each day of week set lesson alarms
-        for(String day  : daysInWeek) {
-            ArrayList<String> lessonData = FileManager.getDataFromDb(this,day);
-            ArrayList<Lesson> lessons = FileManager.getLessonsFromDb(lessonData,day);
-            for(Lesson lesson : lessons) {
-                if(lesson.getAttendance())
-                    NotificationAppManager.setLessonNotification(this, lesson);
-            }
-        }
-    }
 
     class AuthenticationPagerAdapter extends FragmentPagerAdapter {
         private ArrayList<Fragment> fragmentList = new ArrayList<>();
